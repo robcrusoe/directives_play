313d50cb03d8421152d6e5b47795163ddede6961
robcrusoe <arka.sain@aol.com>
2020-10-03 21:18:54 +0530

    Implements Custom Attribute directive using @HostListener() decorator

--- src/app/better-highlight.directive.ts
+++ src/app/better-highlight.directive.ts
@@ -1,18 +1,21 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
 	selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective implements OnInit {
+export class BetterHighlightDirective {
+
+	@HostListener('mouseenter') mouseover(eventData: Event) {
+		this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'orange');
+	}
+
+	@HostListener('mouseleave') mouseremove(eventData: Event) {
+		this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'transparent');
+	}
+
 
 	constructor(
 		private renderer: Renderer2,
 		private elementRef: ElementRef
 	) { }
-
-
-	ngOnInit(): void {
-		this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'orange');
-	}
-
 }
