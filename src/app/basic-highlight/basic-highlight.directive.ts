dbfcc4044b25ae4964cf3b63680dce24e65409aa
robcrusoe <arka.sain@aol.com>
2020-10-03 21:09:24 +0530

    Creates a new Attribute Directive

---
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+    selector: '[appBasicHighlight]'
+})
+export class BasicHighlightDirective implements OnInit {
+
+    constructor(
+        private elementRef: ElementRef
+    ) { }
+
+
+    ngOnInit(): void {
+        this.elementRef.nativeElement.style.backgroundColor = 'green';
+    }
+
+}
