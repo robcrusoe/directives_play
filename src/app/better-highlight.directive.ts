fc72ce34dad747a6f1c1c0fa443735205a72a937
robcrusoe <arka.sain@aol.com>
2020-10-03 21:22:18 +0530

    Implements use of @HostBinding decorator

--- src/app/better-highlight.directive.ts
+++ src/app/better-highlight.directive.ts
@@ -1,16 +1,23 @@
-import { Directive, Renderer2, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
 	selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective {
 
+	@HostBinding('style.backgroundColor') backgroundColorProp: string = 'transparent';
+	@HostBinding('style.color') colorProp: string = 'black';
+
 	@HostListener('mouseenter') mouseover(eventData: Event) {
-		this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'orange');
+		// this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'orange');
+		this.backgroundColorProp = 'blue';
+		this.colorProp = 'whitesmoke';
 	}
 
 	@HostListener('mouseleave') mouseremove(eventData: Event) {
-		this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'transparent');
+		// this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'transparent');
+		this.backgroundColorProp = 'transparent';
+		this.colorProp = 'black';
 	}
 
 
