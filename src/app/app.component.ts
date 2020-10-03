245c0a5106a7b647882e4db34acc1292b7ec6692
robcrusoe <arka.sain@aol.com>
2020-10-03 21:04:39 +0530

    Implements Angular directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  numbers = [1, 2, 3, 4, 5];
+  oddNumbers: number[] = [1, 3, 5];
+  evenNumbers: number[] = [2, 4];
   onlyOdd = false;
 }
